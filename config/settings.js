caa2905997986e88c71248b55ce545321f19a6ec
TheeMorningStar <patelyatri14@gmail.com>
2023-04-13 17:56:20 -0400

    Web Project-Add existing project files to Git

---
+++ config/settings.js
@@ -0,0 +1,13 @@
+/****************************************************************************** ***
+ *	ITE5315 – Project
+ *	I declare that this assignment is my own work in accordance with Humber Academic Policy.   *  No part of this assignment has been copied manually or electronically from any other source *  (including web sites) or distributed to other students.
+ *
+ *	Name: Yatri Patel Gautam Student ID:N01511061 Date: 2023-03-25
+ *
+ *
+ ******************************************************************************
+ **/
+
+module.exports = {
+  mongoDBUrl: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.yma4u3l.mongodb.net/sample_geospatial`,
+};
